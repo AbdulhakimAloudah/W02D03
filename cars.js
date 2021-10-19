bf31c36769b6587189fd5c6c4858d5c8044138c3
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-19 11:46:03 +0300

    kj

---
+++ cars.js
@@ -0,0 +1,19 @@
+class car {
+  constructor(model, colour, image, registration, number, price) {
+    this.model = model;
+    this.colour = colour;
+    this.image = image;
+    this.registration = registration;
+    this.number = number;
+    this.price = price;
+  }
+  editPrice(newPrice) {
+  this.price = newPrice}
+showAll(){
+    return 'The ${thes.name} is ${this.editPrice}rial'
+}
+ 
+};
+  
+//-------
+
