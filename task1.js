37beea92ea823fdf480ad05ed986da7ae757cac7
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-19 17:00:39 +0300

    skj

---
+++ task1.js
@@ -0,0 +1,8 @@
+
+const naa = document.querySelector("#uu");
+naa.style.color = "red";
+
+//----
+const wee = document.querySelector("#eeee");
+wee.addEventListener("click", function xx(){
+  naa.style.fontSize="40px"});
