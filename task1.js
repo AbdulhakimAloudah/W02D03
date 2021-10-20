51387f79fecbebd22a3b60256000a35534e081a1
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-20 10:08:41 +0300

    wv

--- task1.js
+++ task1.js
@@ -1,3 +1,40 @@
+
+
+//----
+const ak = document.querySelector("#btn");
+const ok = document.querySelector("#hh");
+
+ak.addEventListener("click", () => {
+  ok.style.fontSize = "40px";
+});
+///----
+
+const qq = document.querySelector("#fos");
+const rr = document.querySelector("#pra");
+
+qq.addEventListener("click", () => {
+  rr.style.color = "red";
+});
+
+//---
+
+const hjgv = document.querySelector("#pbac");
+const lo = document.querySelector("#pra");
+
+hjgv.addEventListener("click", () => {
+  lo.style.backgroundColor = "green";
+});
+
+///-------
+
+const hime = document.querySelector("#him");
+const loo = document.querySelector("#imgg");
+
+hime.addEventListener("click", () => {
+  loo.style.visibility = "hidden";
+});
+
+//------
 // const wee = document.querySelector("#eeee");
 // wee.addEventListener("click", function xx() {
 //   headings.forEach((naa) => {
@@ -6,10 +43,3 @@
 //const ak = document.querySelectorAll("div");
 //const ak = document.getElementById("btn")
 //const ak = document.getElementByTagName("h1")
-
-const ak = document.querySelector("#btn");
-const ok = document.querySelector("#hh");
-
-ak.addEventListener("click", () => {
-  ok.style.fontSize = "40px";
-});
